99da442b91f65595014c1daa4870cec5a65686d0
Hwan-seok <tttkhs@naver.com>
2023-04-14 03:50:57 +0900

    test: Add test for an InfiniteList

--- test/model/infinite_list_test.dart
+++ test/model/infinite_list_test.dart
@@ -16,18 +16,6 @@ void main() {
     expect(list.isEmpty, isTrue);
   });
 
-  test('removeAt()', () {
-    const list = InfiniteList<int>(items: [1, 2, 3]);
-
-    expect(list.removeAt(1).items, [1, 3]);
-  });
-
-  test('addItems()', () {
-    const list = InfiniteList<int>();
-
-    expect(list.addItems([1, 2, 3, 4, 5]).items, [1, 2, 3, 4, 5]);
-  });
-
   group('reset()', () {
     test('clears all state to initial', () {
       const list = InfiniteList<int>(
@@ -39,5 +27,61 @@ void main() {
 
       expect(list.reset(), const InfiniteList<int>());
     });
+  });
+
+  group('insert()', () {
+    test('inserts a item at the idx', () {
+      const list = InfiniteList<int>(items: [1, 2, 3]);
+      expect(list.insert(1, 0).items, [1, 0, 2, 3]);
+    });
+
+    test('inserts at the end if idx is greater than the length', () {
+      const list = InfiniteList<int>(items: [1, 2, 3]);
+      expect(list.insert(10, 0).items, [1, 2, 3, 0]);
+
+      const list2 = InfiniteList<int>(items: [1, 2, 3]);
+      expect(list2.insert(3, 1).items, [1, 2, 3, 1]);
+    });
+  });
+
+  test('addItem()', () {
+    const list = InfiniteList<int>(items: [1, 2, 3]);
+    expect(list.addItem(5).items, [1, 2, 3, 5]);
+  });
+
+  test('addItems()', () {
+    const list = InfiniteList<int>(items: [1, 2, 3]);
+    expect(list.addItems([7, 8, 9]).items, [1, 2, 3, 7, 8, 9]);
+  });
+
+  group('replace()', () {
+    test('replaces `before` to an `after`', () {
+      const list = InfiniteList<int>(items: [1, 2, 3]);
+      expect(list.replace(2, 4).items, [1, 4, 3]);
+    });
+
+    test('affects nothing if there is no `before` in the list', () {
+      const list = InfiniteList<int>(items: [1, 2, 3]);
+      expect(list.replace(4, 5).items, [1, 2, 3]);
+    });
+  });
+
+  group('replaceAt()', () {
+    test('replaces an item at the idx with the `item`', () {
+      const list = InfiniteList<int>(items: [1, 2, 3]);
+      expect(list.replaceAt(1, 4).items, [1, 4, 3]);
+    });
+
+    test('happens nothing if idx is not bound with items', () {
+      const list = InfiniteList<int>(items: [1, 2, 3]);
+      expect(list.replaceAt(10, 4).items, [1, 2, 3]);
+      expect(list.replaceAt(-1, 4).items, [1, 2, 3]);
+    });
+  });
+
+  test('removeAt()', () {
+    const list = InfiniteList<int>(items: [1, 2, 3]);
+
+    expect(list.removeAt(1).items, [1, 3]);
   });
 }
