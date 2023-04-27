de0936af664864f5fb0ce42f8010ab86e0713975
Hwanseok Barth Kang <tttkhs@naver.com>
2023-04-28 01:21:34 +0900

    feat: Remove unnecessary dependencies and some refactors (#33)

--- lib/src/model/slice.dart
+++ lib/src/model/slice.dart
@@ -1,42 +1,27 @@
 import 'package:equatable/equatable.dart';
-import 'package:json_annotation/json_annotation.dart';
 
-part 'slice.g.dart';
-
-@JsonSerializable(genericArgumentFactories: true)
 class Slice<T> extends Equatable {
-  const Slice(
-    this.content,
-    this.last,
-  );
+  final List<T> items;
+  final bool didFetchedAll;
+
+  bool get isEmpty => items.isEmpty;
+  bool get isNotEmpty => items.isNotEmpty;
+  bool get hasNext => !didFetchedAll;
+
+  const Slice({
+    required this.items,
+    required this.didFetchedAll,
+  });
 
   const Slice.empty()
-      : content = const [],
-        last = false;
-
-  factory Slice.fromJson(
-    Map<String, dynamic> json,
-    T Function(Object? json) fromJsonT,
-  ) =>
-      _$SliceFromJson(json, fromJsonT);
-
-  Map<String, dynamic> toJson(
-    Object Function(T value) toJsonT,
-  ) =>
-      _$SliceToJson(this, toJsonT);
+      : items = const [],
+        didFetchedAll = false;
 
   Slice<R> map<R>(R Function(T element) mapper) => Slice<R>(
-        content.map(mapper).toList(),
-        last,
+        items: items.map(mapper).toList(),
+        didFetchedAll: didFetchedAll,
       );
 
-  final List<T> content;
-  final bool last;
-
-  bool get isEmpty => content.isEmpty;
-  bool get isNotEmpty => content.isNotEmpty;
-  bool get hasNext => !last;
-
   @override
-  List<Object?> get props => [content, last];
+  List<Object?> get props => [items, didFetchedAll];
 }
--- test/model/slice_test.dart
+++ test/model/slice_test.dart
@@ -3,9 +3,12 @@ import 'package:test/test.dart';
 
 void main() {
   test('Slice can be mapped with other type', () {
-    const slice = Slice<String>(['1', '2', '3'], false);
+    const slice = Slice<String>(items: ['1', '2', '3'], didFetchedAll: false);
     final mappedSlice = slice.map(int.parse);
 
-    expect(mappedSlice, const Slice<int>([1, 2, 3], false));
+    expect(
+      mappedSlice,
+      const Slice<int>(items: [1, 2, 3], didFetchedAll: false),
+    );
   });
 }
