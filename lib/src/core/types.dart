997c5a7a2e2b70a35c82382fc169173b880b4740
Hwan-seok <tttkhs@naver.com>
2023-05-01 00:25:39 +0900

    feat: Revert change of optional cancelToken

--- lib/src/core/types.dart
+++ lib/src/core/types.dart
@@ -3,6 +3,6 @@ import 'package:bloc_infinite_list/src/model/slice.dart';
 typedef PagedSliceFetcher<T, S> = Future<Slice<T>> Function(
   int page,
   int size,
-  S state, [
+  S state,
   dynamic cancelToken,
-]);
+);
